9906fd111cd63c239b2decd4646317b002069e54
proXDhiya <prox.dhiya@gmail.com>
2025-04-26 20:43:59 +0100

    Feat: make the exit button work

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def init_screen(width, height):
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
 
