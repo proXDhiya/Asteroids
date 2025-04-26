8c7ad311c78d108bad7dadcb0e9fac12bd342646
proXDhiya <prox.dhiya@gmail.com>
2025-04-26 20:27:58 +0100

    Feat: adding game loop

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from constants import *
 import pygame
+
+def init_screen(width, height):
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # init the game, I HAVE NO IDEA WHAT I AM DOING
+    init_screen(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
